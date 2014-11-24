275fb7160ddc7dc42b663042098e7ed49c182f3f
skmagiik <skmagiik@gmail.com>
2014-11-24 14:41:23 -0600

    Update spawnCost.js

--- spawnCost.js
+++ spawnCost.js
@@ -1,4 +1,5 @@
 /* spawnCost.js
+ * spawnCost.getCost(parts) -- returns cost of body parts in array
  * spawnCost.builder(level) -- returns cost in energy to build each level of a creep
  * MOVE = 50
  * WORK = 20
@@ -6,6 +7,23 @@
  * ATTACK = 100
  * TOUGH = 5
  */
+exports.getCost = function(parts){
+    var sum = 0;
+    var costs = {};
+    costs[Game.MOVE] = 50; 
+    costs[Game.WORK] = 20; 
+    costs[Game.CARRY] = 50;
+    costs[Game.ATTACK] = 100;
+    costs[Game.RANGED_ATTACK] = 150;
+    costs[Game.HEAL] = 200;
+    costs[Game.TOUGH] = 5;
+    if(parts.length){
+        for (var i in parts) {
+            sum += costs[parts[i]];
+        }
+    }
+    return sum;
+}
 exports.builder = function(level){
     if(level === 0 || level === 1){
         return 160; // 3xWORK 1xCARRY 1xMOVE
